4c8a0f6760279f523eb5b87cf87e45067373af41
Dungeon master Winnervello <kislicyntima@gmail.com>
2024-09-17 21:20:47 +0700

    com3

--- src/test/java/fizzbuzzTest.java
+++ src/test/java/fizzbuzzTest.java
@@ -3,11 +3,11 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class fizzbuzzTest {
-
     @Test
     void fizzbuzz() {
         assertEquals("fizz", fizzbuzz.fizzbuzz(5));
         assertEquals("buzz", fizzbuzz.fizzbuzz(7));
         assertEquals("1", fizzbuzz.fizzbuzz(1));
+        assertEquals("fizzbuzz", fizzbuzz.fizzbuzz(490));
     }
 }
